5406c994153b38e71324a984214a39b31c368a23
akhilviradiya <137093758+akhilviradiya@users.noreply.github.com>
2023-07-14 10:35:03 +0530

    Add files via upload

---
+++ operator.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 100;  
+$y = 56;
+$x+=20;
+
+var_dump($x == $y); 
+var_dump($x != $y);
+ 
+echo $x; //120(100+20)
+
+echo"<br>";
+
+if ($x==120 and $y==56){
+    echo "helllo";
+}
+?>  
+
+</body>
+</html>
---
+++ variable_function.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $x = 56;
+        function akki() {
+           // echo "value is : $x";  : it's give error because it is inside function 
+            
+        }
+        akki();
+
+        echo "value is : $x";
+    ?>
+</body>
+</html>
